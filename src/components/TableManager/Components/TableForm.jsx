e256e611238f15c6c200daf5f09cc9fc80a5826a
eeee386 <attilasedon1@gmail.com>
2019-01-14 22:14:05 +0100

    Form started

--- src/components/TableManager/Components/TableForm.jsx
+++ src/components/TableManager/Components/TableForm.jsx
@@ -1,19 +1,32 @@
 import React, { Component } from 'react'
 import {Form, Field, reduxForm} from 'redux-form';
+import s from './TableForm';
 
 const TABLE_FORM_NAME = 'TABLE_FORM';
 
 class TableForm extends Component {
+
+  renderFormInput = (name, type) => {
+    return (
+      <div>
+        <label>{name}</label>
+        <input type={type} />
+      </div>
+    )
+  }
+
   render() {
     return (
       <Form onSubmit={this.props.handleSubmit}>
-        <Field name="date" component="input" type="text" />
-        <Field name="name" component="input" type="text" />
-        <Field name="address" component="input" type="text" />
-        <Field name="taxID" component="input" type="text" />
-        <Field name="originID" component="input" type="text" />
-        <Field name="HLF" component="input" type="text" />
-        <Field name="weightInKilograms" component="input" type="text" />
+        <div className={s.root}>        
+          <Field name="date" component={() => this.renderFormInput("Dátum", "text")} />
+          <Field name="name" component={() => this.renderFormInput("Név", "text")} />
+          <Field name="address" component={() => this.renderFormInput("Lakcím", "text")} />
+          <Field name="taxID" component={() => this.renderFormInput("AdóSzám", "text")} />
+          <Field name="originID" component={() => this.renderFormInput("Származási Szám", "text")} />
+          <Field name="HLF" component={() => this.renderFormInput("Dátum", "text")} />
+          <Field name="weightInKilograms" component={() => this.renderFormInput("Dátum", "text")} />
+        </div>
       </Form>
     )
   }
