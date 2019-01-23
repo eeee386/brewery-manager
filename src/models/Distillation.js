23d404735954b0ad6a4394a7083d174e630844d7
eeee386 <attilasedon1@gmail.com>
2019-01-23 22:39:20 +0100

    barebones webpack, and typescript, react files

--- src/models/Distillation.js
+++
@@ -1,9 +0,0 @@
-export function DistillationModel(date, name, address, taxID, originID, HLF, weightInKilograms) {
-    this.date = date;
-    this.name = name;
-    this.address = address;
-    this.taxID = taxID;
-    this.originID = originID;
-    this.HLF = HLF;
-    this.weightInKilograms = weightInKilograms;
-}
